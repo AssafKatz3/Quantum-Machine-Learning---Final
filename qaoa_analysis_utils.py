a79ccb39d3670d8d942a7562f1983bad02a64ec4
Assaf Katz <assaf.katz@bynet.co.il>
2023-08-30 14:47:44 +0300

    Fix control noise

--- qaoa_analysis_utils.py
+++ qaoa_analysis_utils.py
@@ -1,4 +1,4 @@
-import multiprocessing
+import concurrent.futures
 
 # Function to run optimal parameters for a QAOA analysis instance
 def run_optimal_params(analysis):
@@ -14,19 +14,3 @@ def run_optimal_params(analysis):
     shot_amt = analysis.simulator.shot_amt
     opt_res_counts = analysis.run_optimal_params()
     return shot_amt, opt_res_counts
-
-# Function to process a list of QAOA analysis instances in parallel
-def process_analysis_list(analysis_list):
-    """
-    Executes the optimal parameter runs for a list of QAOA analysis instances in parallel using multiprocessing.
-
-    Parameters:
-        analysis_list (List[QAOAAnalysis]): A list of QAOA analysis instances to be processed in parallel.
-
-    Returns:
-        opt_res_counts_dict_ns (dict): A dictionary containing the simulation results of optimal parameter runs for different shot amounts.
-    """
-    opt_res_counts_dict_ns = {}  # Create a dictionary to store results for this analysis_list
-    for qaoa_analysis in analysis_list:
-        opt_res_counts_dict_ns[qaoa_analysis.simulator.shot_amt] = run_optimal_params(qaoa_analysis)
-    return opt_res_counts_dict_ns
